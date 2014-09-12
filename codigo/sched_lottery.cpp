88091d83dc8dfb59c2437fe0977aa0eae9b63ab2
Matias <matias@ubuntu.(none)>
2014-09-12 18:57:35 -0300

    codigo lottery

--- codigo/sched_lottery.cpp
+++ codigo/sched_lottery.cpp
@@ -1,30 +1,158 @@
 #include "sched_lottery.h"
+#include <cstdlib>
 
 using namespace std;
 
-SchedLottery::SchedLottery(vector<int> argn) 
+SchedLottery::SchedLottery(vector<int> argn) : semilla(argn[1]), quantum(argn[2]) 
 {
-  // FCFS recibe la cantidad de cores.
+	compensatorio = 0;
 }
 
 SchedLottery::~SchedLottery() 
 {
+
 }
 
 void SchedLottery::load(int pid) 
 {
-  load(pid,0);
+  	// Como llega una nueva tarea al scheduler, se debe recalcular la cantidad de tickets de todas las tareas
+  	// para mantener equitativa la probabilidad. 
+
+  	int cantTareas = tareasYTickets.size() + 1;
+
+  	// Ahora dividimos los 100 tickets por la cantidad de Tareas
+
+  	int ticketsCadaUno = 100 / cantTareas;
+
+  	int resto = 100 % cantTareas; // Al resto lo repartimos entre todas
+
+  	//metemos el nuevo pid
+
+  	tareasYTickets.insert(std::pair<int,int>(pid, ticketsCadaUno));
+
+  	bloqueadasTerminadas.insert(std::pair<int,bool>(pid, 1));
+
+  	//Actualizamos la cantidad de tickets de todos
+
+  	std::map<int,int>::iterator it;
+
+  	for (it=tareasYTickets.begin(); it!=tareasYTickets.end(); ++it)
+  	{
+  		it->second =ticketsCadaUno;
+
+  		//reparto el resto, un ticket para cada uno hasta que se termine
+
+  		if(resto>0)
+  		{
+  			it->second++;
+  			resto--;
+  		}
+
+  	}
+
 }
 
 void SchedLottery::load(int pid,int deadline) 
 {
+	tareasYTickets.insert(std::pair<int,int>(pid, 1));
 }
 
 void SchedLottery::unblock(int pid) 
 {
+	std::map<int,bool>::iterator it = bloqueadasTerminadas.find(pid);
+
+	it->second = 1;
+
 }
 
-int SchedLottery::tick(int cpu, const enum Motivo m) 
+int SchedLottery::tick(int cpu, const enum Motivo motivo) 
 {
-  return 0;
+
+	if(compensatorio)
+	{//modo compensation tickets
+		return 0;	
+	}
+	else
+	{// modo basico
+
+
+		if (motivo == TICK)
+		{
+			int tareaActual = current_pid(cpu);
+			//Si estoy en la IDLE me la salteo
+			if (tareaActual == IDLE_TASK && !tareasYTickets.empty()) 
+				return loteria();
+
+			cantTicks++;
+			
+			if (cantTicks == quantum)
+			{//si me pase del quantum se resetean la cuenta de ticks y elijo otra tarea	
+				cantTicks = 0;
+				
+				return loteria();
+			}
+			// si no me pase del quantum sigue la actual
+			return tareaActual;
+		}
+		else if (motivo == EXIT || motivo == BLOCK)
+		{
+			int tareaActual = current_pid(cpu);
+			cantTicks++;
+
+			//Bloqueo o termino la tarea
+
+			std::map<int,bool>::iterator it = bloqueadasTerminadas.find(tareaActual);
+
+			it->second = 0;
+			
+			if(tareasYTickets.empty())
+			{//si se acabaron las tareas
+				return IDLE_TASK;
+			}
+			else
+			{//si no eljio otra tarea
+				return loteria();	
+			}
+
+		}
+	
+	}
+	
+}
+	
+
+
+int SchedLottery::loteria()
+{
+	std::srand(semilla); 
+
+	int ticketGanador = rand() % 100;
+
+	bool estaDisponible;
+
+	do
+	{
+		int duenioTicketGanador;
+
+		std::map<int,int>::iterator it;
+
+		int sumaParcial = 0;
+
+  		while (it!=tareasYTickets.end() && (sumaParcial < ticketGanador))
+  		{
+  			sumaParcial += it-> second;
+
+  			if(ticketGanador <= sumaParcial)
+  			{
+  				duenioTicketGanador = it->first;
+  				return duenioTicketGanador;
+  			}
+
+  			++it;
+  		}
+
+
+  	estaDisponible = (bloqueadasTerminadas.find(duenioTicketGanador))->second;
+
+	}while(!estaDisponible);
 }
--- codigo/sched_lottery.h
+++ codigo/sched_lottery.h
@@ -2,7 +2,7 @@
 #define __SCHED_LOT__
 
 #include "basesched.h"
-
+#include <map>
 
 class SchedLottery : public SchedBase {
   public:
@@ -13,6 +13,23 @@ class SchedLottery : public SchedBase {
     virtual void unblock(int pid);
     virtual int tick(int cpu, const enum Motivo m);
 
+    private:
+		int loteria();
+		// el valor de quantum por parametro
+		int quantum;
+		// la cantidad de ticks actuak
+		int cantTicks;
+		int semilla;
+		std::map<int, int> tareasYTickets;
+		bool compensatorio;
+		/* Para cambiar en el constructor:
+		 0 = modo basico
+		 1 = modo basico + compensation tickets
+		*/
+		bool hayCompensado;
+		//Para saber si el proceso current_pid fue compensado en el tick que paso
+		std::map<int, bool> bloqueadasTerminadas;
+		//Para saber cuales tareas fueron bloqueadas o ya terminaron (1 == ready, 0 == block o exit)
 };
 
 #endif
