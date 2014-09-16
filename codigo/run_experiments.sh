f19962e432fc7bece0b92ccc7b35e4770c65b5b8
Sebastian Torres <softmindfreak@gmail.com>
2014-09-16 20:19:00 -0300

    Update run_experiments

--- codigo/run_experiments.sh
+++ codigo/run_experiments.sh
@@ -5,7 +5,7 @@
 N="1"
 while [ $N -lt $1 ];
 do
-    echo "Creating ej9_${N}.png experiment..."
-    ./simusched Tareas/lote$2.tsk 1 1 0 SchedLottery $3 $RANDOM | ./graphsched.py > ../latex/Graficos/ej9_${N}.png
+    echo "Creating ej9_${N}_${4}.png experiment..."
+    ./simusched Tareas/lote$2.tsk 1 1 0 SchedLottery$4 $3 $RANDOM | ./graphsched.py > ../latex/Graficos/ej9_${N}_bis.png
     N=$[ $N+1 ]
 done
